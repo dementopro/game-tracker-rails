ccd30fdaff26b3cae1122bd10d1b8a674492f658
dementopro <dementopro47@gmail.com>
2017-07-24 16:23:58 +0100

    add more actions to games controller

--- board_game_api/app/controllers/games_controller.rb
+++ board_game_api/app/controllers/games_controller.rb
@@ -7,7 +7,52 @@ class GamesController < ApplicationController
 
   def show
     game = Game.find( params[:id] )
-    render( json: game.as_json( { include: :players } ) )
+    render(
+      json: game.to_json( {
+        only: [:title, :id],
+         include: {
+            wins: {
+              include: {
+                player: {
+                  only: [:name]
+                }
+              },
+              only: [:player_id]
+            }
+          }
+        }
+      )
+    )
+  end
+
+  def create
+   game = Game.create( game_params )
+   render json: game, status: :created
+  end
+
+  def update
+    game = Game.find( params[:id] )
+    if game.update_attributes(game_params)
+      render json: game
+    else
+      render json: {status: :update_failed}
+    end
+  end
+
+  def destroy
+    game = Game.find( params[:id])
+    if game.destroy!
+      render json: {status: :success}
+    else
+      render json: {status: :delete_failed}
+    end
+  end
+
+
+
+  private
+  def game_params
+    params.require(:game).permit([:title])
   end
 
 end
