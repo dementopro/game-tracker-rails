3f53fae700b67e1dff97f0c2a5bba8386b2210a1
dementopro <dementopro47@gmail.com>
2017-07-26 16:04:43 +0100

    can add new win

--- react/client/src/components/NewWinForm.jsx
+++ react/client/src/components/NewWinForm.jsx
@@ -5,10 +5,15 @@ class NewWinForm extends React.Component{
   constructor(props){
     super(props)
       this.state ={
+        players: [],
         wins: [],
+        games: [],
         playerIndex: "",
         focusPlayer: null,
+        playerId: "",
+        gameId:"",
         gameIndex: "",
+        dateIndex: "",
         focusGame: null
     }
   }
@@ -27,62 +32,84 @@ class NewWinForm extends React.Component{
      request.send(null);
    }
 
-   addNewWin(date) {
+  getPlayerList() {
+     const url = 'http://localhost:5000/api/players/'
+     const request = new XMLHttpRequest();
+     request.open('GET', url);
+     request.onload = () => {
+       if (request.status === 200) {
+         const jsonData = request.responseText;
+         const playerData = JSON.parse(jsonData);
+         this.setState({
+           players: playerData,
+           playerId: playerData[0].id
+         });
+       }
+     }
+     request.send(null);
+   }
+
+   getGameList() {
+      const url = 'http://localhost:5000/api/games/'
+      const request = new XMLHttpRequest();
+      request.open('GET', url);
+      request.onload = () => {
+        if (request.status === 200) {
+          const jsonData = request.responseText;
+          const gameData = JSON.parse(jsonData);
+          this.setState({
+            games: gameData,
+            gameId: gameData[0].id
+          });
+        }
+      }
+      request.send(null);
+    }
+
+   addNewWin(date, playerId, gameId) {
      const url = 'http://localhost:5000/api/wins/'
-     const newWin = {win: {date: date } }
+     const newWin = {win: {date: date, player_id: playerId, game_id: gameId} }
      const request = new XMLHttpRequest();
      request.open('POST', url);
      request.setRequestHeader("Content-Type", "application/json")
-     request.onload = () => {
-       this.getPlayerList()
-     }
-     request.send(JSON.stringify(newPlayer))
+     request.send(JSON.stringify(newWin))
    }
 
    componentDidMount(){
-     this.getWinList()
+     this.getPlayerList()
+     this.getGameList()
    }
 
    submitNewWinner(event){
      event.preventDefault()
-     this.addNewWin(this.state.date)
+     this.addNewWin(this.state.date, this.state.playerId, this.state.gameId)
    }
 
    handlePlayerSelect(event){
      const newIndex = event.target.value;
-     this.setState({playerIndex: newIndex})
-
-     const selectedPlayer = this.wins.players[newIndex]
-     this.setFocusPlayer(selectedPlayer)
-   }
-
-   setFocusPlayer(player){
-     this.setState({focusPlayer: player})
+     this.setState({playerId: newIndex})
    }
 
    handleGameSelect(event){
      const newIndex = event.target.value;
-     this.setState({gameIndex: newIndex})
-
-     const selectedGame = this.wins.games[newIndex]
-     this.setFocusGame(selectedGame)
+     this.setState({gameId: newIndex})
    }
 
-   setFocusGame(game){
-     this.setState({focusGame: game})
+   handleDate(event){
+     const newIndex = event.target.value;
+     this.setState({date: newIndex})
    }
 
 
    render() {
-    const playerOptions = this.state.wins.map((win, index) => {
-      return <option value={index} key={index}>{win.player.name}</option>
+     console.log(this.state);
+    const playerOptions = this.state.players.map((player, index) => {
+      return <option value={player.id} key={index}>{player.name}</option>
     })
 
-    const gameOptions = this.state.wins.map((win, index) => {
-      return <option value={index} key={index}>{win.game.title}</option>
+    const gameOptions = this.state.games.map((game, index) => {
+      return <option value={game.id} key={index}>{game.title}</option>
     })
-
-     console.log(this.state.wins, "wins")
 
      return (
        <div>
@@ -90,14 +117,14 @@ class NewWinForm extends React.Component{
          <li><Link to="/Players">Players</Link></li>
          <li><Link to="/Games">Games</Link></li>
          <h2>Add a new winner!</h2>
-         <form onSubmit={this.submitNewWinner}>
-        <select id= "player-select" name="select_player">
+         <form onSubmit={this.submitNewWinner.bind(this)}>
+        <select id= "player-select" name="select_player" onChange={this.handlePlayerSelect.bind(this)}>
           {playerOptions}
         </select>
-        <select id="game-select">
+        <select id="game-select" onChange={this.handleGameSelect.bind(this)}>
           {gameOptions}
         </select>
-        <input type="date"></input>
+        <input type="date" onChange={this.handleDate.bind(this)}></input>
         <input type="submit" name="submit" value="Add New Win!" />
         </form>
         </div>
