b1d7df33dbcc7b8c250b92323cfa80f60689057f
dementopro <dementopro47@gmail.com>
2017-07-27 00:11:45 +0100

    fix syntax error

--- board_game_api/app/models/game.rb
+++ board_game_api/app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
 
   has_many :wins
-  has_many :players, through: :wins :dependent => :delete_all
+  has_many :players, through: :wins, :dependent => :delete_all
 
 end
